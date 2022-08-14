d5a863408f5190634dbfe18f833c0d6b9a871966
romantafelski <romantafelski18@gmail.com>
2022-08-14 13:19:42 -0400

    dsf

--- server.js
+++ server.js
@@ -81,7 +81,20 @@ app.get("/new", (req, res) => {
 })
 
 
-
+app.post("/", (req, res) => {
+    Soccer.create(req.body, (err, createdPlayer) => {
+      res.redirect("/")
+    })
+  })
+  
+//   app.get("/:id", (req, res) => {
+//       Soccer.findById(req.params.id, (err, foundPlayer) => {
+//         res.render(show.ejs, {
+//           soccer:foundPlayer
+//         })
+//       })
+    
+//   })
 
 
 
